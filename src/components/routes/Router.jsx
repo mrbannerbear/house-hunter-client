1de0cbeb2657935c9c1a759468e777cb6b28fcc6
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-01-23 14:03:05 +0600

    Initialized react router

---
+++ src/components/routes/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = new createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <>hello</>
+        }
+    ]
+)
+
+export default routes
